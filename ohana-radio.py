6c711b04e1a4653d6ba39bf35dfce122271e7070
Rada Berar <rada.berar@ohanacode-dev.com>
2021-03-04 11:54:43 +0100

    port 80

--- ohana-radio.py
+++ ohana-radio.py
@@ -8,7 +8,7 @@ import json
 from bluetooth_ctrl import BluetoothController
 
 WEB_PORT = 9000
-#WEB_PORT = 80 	# Use this as root
+WEB_PORT = 80 	# Use this as root
 
 SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 PLS_PATH = os.path.join(os.path.expanduser('~'), 'playlists')
